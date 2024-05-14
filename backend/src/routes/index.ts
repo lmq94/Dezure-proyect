336969884afd43f3a6e29c5b318715dc499ae104
Lucas Quiroga <quiroga9409@gmail.com>
2024-05-14 20:16:45 -0300

    Add jwt security to endpoints

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,21 +5,29 @@ import UserController from "../controller/UserController";
 import UserService from "../service/UserService";
 import ProductService from "../service/ProductService";
 import ProductController from "../controller/ProductController";
+import AuthService from "../service/AuthService";
+import verifyToken from "../middleware/auth";
 
 
 const router = Router();
 const userService = new UserService();
 const productService: ProductService = new ProductService();
- let userController = new UserController(userService);
+const authService = new AuthService(userService);
+ let userController = new UserController(userService, authService);
  let productController = new ProductController(productService);
 
 
-router.get('/users/:id', userController.getUserById.bind(userController));
-router.post('/users', userController.createUser.bind(userController))
-router.patch('/users/:id', userController.updateUser.bind(userController))
-router.delete('/users/:id', userController.deleteUser.bind(userController))
 
-router.get('/products', productController.getAllProducts.bind(productController))
-router.post('/products', productController.createProduct.bind(productController))
+router.get('/users/:id', verifyToken, userController.getUserById.bind(userController));
+router.post('/users', verifyToken, userController.createUser.bind(userController))
+router.patch('/users/:id', verifyToken, userController.updateUser.bind(userController))
+router.delete('/users/:id', verifyToken, userController.deleteUser.bind(userController))
+
+router.post('/login', userController.login.bind(userController));
+
+router.get('/products', verifyToken, productController.getAllProducts.bind(productController))
+router.post('/products', verifyToken, productController.createProduct.bind(productController))
+router.patch('/products/:id', verifyToken, productController.updateProduct.bind(productController))
+router.delete('/products/:id', verifyToken, productController.deleteProduct.bind(productController))
 
 export default router;
