5d3284799c9d75926133cbd1076cb0578b397bd1
Lucas Quiroga <quiroga9409@gmail.com>
2024-05-14 20:12:50 -0300

    Remove innecesary code

--- backend/config/dataBase.ts
+++ backend/config/dataBase.ts
@@ -2,13 +2,12 @@ import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 
-// Cargar variables de entorno desde .env
 config({ path: resolve(process.cwd(), `.env.${process.env.NODE_ENV || 'development'}`) });
 
 const configPath = resolve(process.cwd(), 'config', 'config.json');
 
 const sequelize = new Sequelize({
-  ...require(configPath)[process.env.NODE_ENV || 'development']
+  ...require(configPath)['development']
 });
 
 export default sequelize;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor Express iniciado en el puerto ${PORT}`);
 });
