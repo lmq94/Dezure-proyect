32156ffbf45f8fc297a71749add6cf40b5aa4a29
Lucas Quiroga <quiroga9409@gmail.com>
2024-05-14 20:17:46 -0300

    Add functionality for product

--- backend/src/controller/ProductController.ts
+++ backend/src/controller/ProductController.ts
@@ -3,10 +3,8 @@ import { Request, Response } from 'express';
 
 
 import ProductService from "../service/ProductService";
-import UserModel from "../model/UserModel";
 import ProductModel from "../model/ProductModel";
-
-
+import ProductDTO from "../interface/ProductDTO";
 
 class ProductController {
     private productService: ProductService;
@@ -16,31 +14,85 @@ class ProductController {
     }
     async getAllProducts(req: Request, res: Response) {
         try {
-            let products = await this.productService.findAllProduct();
-            return res.json(products);
+            const { page, filterField, filterValue } = this.extractQueryParameters(req);
+            const pageSize = 10;
+
+            let products: { totalItems: number; productsDTO: ProductDTO[] };
+            if (filterField) {
+                products = await this.productService.getFilteredProducts(page, pageSize, filterField, filterValue);
+            } else {
+                products = await this.productService.getAllProducts(page, pageSize);
+            }
+
+            const totalPages = Math.ceil(products.totalItems / pageSize);
+
+            if (page < 1 || page > totalPages) {
+                return res.status(404).json('La página solicitada no existe');
+            }
+
+            return res.json({
+                page,
+                pageSize,
+                totalItems: products.totalItems,
+                totalPages,
+                products: products.productsDTO,
+            });
         } catch (error) {
             console.error('Error al obtener productos:', error);
             return res.status(500).json({ message: 'Error al obtener productos' });
         }
     }
 
+    private extractQueryParameters(req: Request) {
+        const page = parseInt(req.query.page as string) || 1;
+        const filterField = req.query.filterField as string;
+        const filterValue = req.query.filterValue as string;
+        return { page, filterField, filterValue };
+    }
+
+
+
     public async createProduct(req: Request, res: Response): Promise<Response> {
-        try {
-            const { name, description, price, stock_quantity, category} = req.body;
+            try {
+                const { name, description, price, stock_quantity, category} = req.body;
 
-            if (!name || !description || !price || stock_quantity || category) {
-                return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+                if (!name || !description || !price || !stock_quantity || !category) {
+                    return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+                }
+
+                const newProduct: ProductModel = await ProductModel.create({ name, description, price, stock_quantity, category });
+
+                return res.status(201).json(newProduct);
+
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Error al crear el producto' });
             }
+    }
 
-            const newProduct: ProductModel = await ProductModel.create({ name, description, price, stock_quantity, category });
-
-            return res.status(201).json(newProduct);
-
+    async updateProduct(req: Request, res: Response) {
+        const product_id = parseInt(req.params.id);
+        const updatedProductData = req.body;
+        try {
+            const updatedUser = await this.productService.updateProduct(product_id, updatedProductData);
+            res.json(updatedProductData);
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'Error al crear el producto' });
+            res.status(500).json({ error: error.message });
         }
     }
+
+    async deleteProduct(req: Request, res: Response) {
+        const product_id = parseInt(req.params.id);
+        try {
+            await this.productService.deleteProduct(product_id);
+            res.json({ message: 'Usuario eliminado correctamente' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+
+
 }
 
 export default ProductController;
--- backend/src/service/ProductService.ts
+++ backend/src/service/ProductService.ts
@@ -1,5 +1,7 @@
 
 import ProductModel from "../model/ProductModel";
+import {Op} from "sequelize";
+import ProductDTO from "../interface/ProductDTO";
 
 
 class ProductService{
@@ -7,12 +9,67 @@ class ProductService{
     constructor() {
     }
 
-    async findAllProduct(): Promise<ProductModel[]> {
-        try {
-            return await ProductModel.findAll();
-        } catch (error) {
+    private productToDTO(productModel: any): ProductDTO {
+        return {
+            category: productModel.category,
+            description: productModel.description,
+            stock_quantity: productModel.stock_quantity,
+            name: productModel.name,
+            price: productModel.price
+        };
+    }
 
-            throw new Error('Error al buscar los productos ' + error.message);
+
+    async getAllProducts(page: number, pageSize: number) {
+        const offset = (page - 1) * pageSize;
+        const { count, rows: products } = await ProductModel.findAndCountAll({
+            limit: pageSize,
+            offset,
+        });
+        const productsDTO = products.map(product => this.productToDTO(product));
+        return { productsDTO, totalItems: count };
+    }
+
+    async getFilteredProducts(page: number, pageSize: number, filterField: string, filterValue: string) {
+        const offset = (page - 1) * pageSize;
+        const where: any = {};
+        if (filterField === 'name') {
+            where[filterField] = { [Op.like]: `%${filterValue}%` };
+        } else {
+            where[filterField] = filterValue;
+        }
+        const { count, rows: products } = await ProductModel.findAndCountAll({
+            where,
+            limit: pageSize,
+            offset,
+        });
+
+        const productsDTO = products.map(product => this.productToDTO(product));
+        return { productsDTO, totalItems: count };
+    }
+
+
+    async updateProduct(id_product: number, updatedProductData: any): Promise<ProductModel | null> {
+        try {
+            const product = await ProductModel.findByPk(id_product);
+            if (!product) {
+                throw new Error('Producto no encontrado');
+            }
+            return await product.update(updatedProductData);
+        } catch (error) {
+            throw new Error('Error al actualizar producto');
+        }
+    }
+
+    async deleteProduct(id_product: number): Promise<void> {
+        try {
+            const product = await ProductModel.findByPk(id_product);
+            if (!product) {
+                throw new Error('Producto no encontrado');
+            }
+            await product.destroy();
+        } catch (error) {
+            throw new Error('Error al eliminar producto');
         }
     }
 
